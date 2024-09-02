0b09a501b4db62b6a1d4511cc230194dbc04e764
nadia <nadia0ostapyshyn@gmail.com>
2024-09-02 20:25:58 +0300

    update type User

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,7 @@ type User = {
   name: string;
   username: string;
   email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
   phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
 };
 
 function App() {
