db241c90bd548ab133067e155c3860d06ad1da5c
nadia <nadia0ostapyshyn@gmail.com>
2024-09-02 17:27:25 +0300

    implement user data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,48 @@
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
 function App() {
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data: User[] = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error("Failed to fetch users:", error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return <div>hello</div>;
 }
 
